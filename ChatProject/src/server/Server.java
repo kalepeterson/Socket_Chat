b2459774244036001e78c3923558b79b0c3f5227
kalepeterson <kalepeterson@unomaha.edu>
2015-11-15 20:43:20 -0600

    Kale - Cleaned up ConnectionsManager and added it to the project.  Created a skeleton for the basic structure of the Server application.  Also added a User class for representing chat participants.

--- ChatProject/src/server/Server.java
+++ ChatProject/src/server/Server.java
@@ -1,10 +1,14 @@
 package server;
 
+import socketChat.SharedLibrary;
+
 /**
  * Created by Kale on 11/15/2015.
  */
 public class Server {
     public static void main(String[] args) {
         System.out.println("I'm a server!");
+        Session[] sessions = new Session[SharedLibrary.MAX_SESSIONS];
+        sessions[0] = new Session("localhost",55601); //default session
     }
 }
